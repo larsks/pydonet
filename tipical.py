6c020d4ee9d76cc242804f80f8e6dc0552997422
larsks <larsks@664d621b-db46-0410-82af-d5443b94c946>
2008-02-27 19:41:56 +0000

    added code for .PKT parsing
    git-svn-id: http://pydonet.googlecode.com/svn/trunk@9 664d621b-db46-0410-82af-d5443b94c946

--- tipical.py
+++ tipical.py
@@ -4,22 +4,24 @@ import os, sys, optparse
 import tic
 
 def parse_args():
-	parser = optparse.OptionParser()
-	parser.add_option('-d', '--inbound')
-	parser.add_option('-a', '--areas')
-	parser.add_option('-l', '--list')
-	parser.add_option('-i', '--import')
+  parser = optparse.OptionParser()
+  parser.add_option('-d', '--inbound')
+  parser.add_option('-a', '--areas')
+  parser.add_option('-l', '--list')
+  parser.add_option('-i', '--import')
 
-	opts, args = parser.parse_args()
-	return opts,args
+  opts, args = parser.parse_args()
+  return opts,args
 
 def main ():
-	opts, args = parse_args()
+  opts, args = parse_args()
 
-	for item in [x for x in os.listdir(opts.inbound) if x.endswith('.TIC')]:
-		t = tic.TIC(os.path.join(opts.inbound, item))
-		if t.valid:
-			print '%(area)s/%(file)s from %(origin)s: %(desc)s' % t
+  for item in [x for x in os.listdir(opts.inbound) if x.endswith('.TIC')]:
+    t = tic.TIC(os.path.join(opts.inbound, item))
+    if t.valid:
+      print '%(area)s/%(file)s origin %(origin)s: %(desc)s' % t
+      print '  received from %(from)s' % t
+      print '  %(size)s bytes' % t
 
 if __name__ == '__main__': main()
 

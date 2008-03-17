9faa985c4b0b24f2a2042405b0aa56de361a7c33
larsks <larsks@664d621b-db46-0410-82af-d5443b94c946>
2008-03-17 02:03:05 +0000

    rewrote sample scripts to use module main() methods
    git-svn-id: http://pydonet.googlecode.com/svn/trunk@28 664d621b-db46-0410-82af-d5443b94c946

--- scripts/pktscan.py
+++ scripts/pktscan.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python
 
-import os, sys, pprint
 from pydonet import packet
-from pydonet import message
-
-for x in sys.argv[1:]:
-  p = packet.PacketFactory(open(x).read())
-  print p
-
-  for m in p.messages:
-    print m
-    m = message.Message(m)
-    print ' ', m
-    pprint.pprint( m.kludge )
+packet.main()
 
